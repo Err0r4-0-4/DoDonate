a87d2ca14b3c8fece181e5a45b1461a0cc687afa
Aditya Singh <201952202@iiitvadodara.ac.in>
2021-09-25 16:14:20 +0530

    Main Page Hospital

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import { useSelector } from "react-redux";
+import Routehos from "./Routehos";
+import Loginpage from "./components/Login/Loginpage";
+import { Route, useLocation, Switch } from "react-router";
 
+import "./App.css";
 function App() {
+  const counter = useSelector((state) => state.counter);
+  const name = useSelector((state) => state.name);
+
+  const loginscreen = (
+    <Switch>
+      <Route path="/">
+        <Loginpage />
+      </Route>
+    </Switch>
+  );
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* <Route path="/">
+        <Home />
+      </Route> */}
+
+      {/* <Routehos></Routehos>
+<Routeuser />
+<Routeadmin /> */}
+      {/* 
+      {counter ? (
+        name ? (
+          <Routehos></Routehos>
+        ) : (
+          <Routeuser />
+        )
+      ) : name ? (
+        <Routeadmin />
+      ) : (
+        loginscreen
+      )} */}
+
+      {counter && name ? <Routehos></Routehos> : loginscreen}
     </div>
   );
 }
